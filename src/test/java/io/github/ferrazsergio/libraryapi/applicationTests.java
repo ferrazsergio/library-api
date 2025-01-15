a3031e0cde55776eeaf3543fc2b382c3d3c7ca43
ferrazsergio <juniorsilva6677@hotmail.com>
2025-01-15 01:50:02 -0300

    Iniciando projeto

---
+++ src/test/java/io/github/ferrazsergio/libraryapi/applicationTests.java
@@ -0,0 +1,13 @@
+package io.github.ferrazsergio.libraryapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class applicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
