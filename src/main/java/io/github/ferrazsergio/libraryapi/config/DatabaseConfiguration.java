2a997f728ac331d62524af2bc0c10f98a754fdfa
ferrazsergio <juniorsilva6677@hotmail.com>
2025-02-26 16:46:15 -0300

    Configurando datasource e melhorando docker

---
+++ src/main/java/io/github/ferrazsergio/libraryapi/config/DatabaseConfiguration.java
@@ -0,0 +1,50 @@
+package io.github.ferrazsergio.libraryapi.config;
+
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+import org.hibernate.Hibernate;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+
+import javax.sql.DataSource;
+
+@Configuration
+public class DatabaseConfiguration {
+    @Value("${spring.datasource.url}")
+    private String url;
+    @Value("${spring.datasource.username}")
+    private String username;
+    @Value("${spring.datasource.password}")
+    private String password;
+    @Value("${spring.datasource.driver-class-name}")
+    private String driver;
+
+    @Bean
+    public DataSource dataSource() {
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        dataSource.setUrl(url);
+        dataSource.setUsername(username);
+        dataSource.setPassword(password);
+        dataSource.setDriverClassName(driver);
+        return dataSource;
+    }
+
+    @Bean
+    public DataSource hikariDataSource() {
+        HikariConfig config = new HikariConfig();
+        config.setJdbcUrl(url);
+        config.setUsername(username);
+        config.setPassword(password);
+        config.setDriverClassName(driver);
+
+        config.setMaximumPoolSize(10); //Maximo de conexoes liberadas
+        config.setMinimumIdle(1); //tamanho inicial do pool
+        config.setPoolName("library-db-pool");
+        config.setMaxLifetime(600000); // 600 mil ms
+        config.setConnectionTimeout(100000); // timeout para conseguir uma conexão
+        config.setConnectionTestQuery("SELECT 1"); //Query test
+        return new HikariDataSource(config);
+    }
+}
